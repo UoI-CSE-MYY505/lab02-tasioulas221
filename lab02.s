665454fa436623b5c2cbce41671345b33569a956
tasioulas221 <cs05490@uoi.gr>
2024-10-18 14:30:43 +0300

    Add files via upload
    Completed lab02

--- lab02.s
+++ lab02.s
@@ -11,6 +11,19 @@ array: .word 1, 0, 1, 12, 0, 1, 4
 prog:
 #-----------------------------
 # Write your code here!
+    add s0, zero, zero
+    add t1 , zero , zero
+loop:
+    beq t1 , a2 ,done
+    slli t2 , t1 , 2
+    add t2 , t2, a0
+    lw t3 , 0(t2)
+    bne t3, a2 , else
+    add s0 , t2 , zero
+else: 
+    addi t1 ,t1 , 1
+    j loop
+
 # Do not remove the prog label or write code above it!
 #-----------------------------
 done:
